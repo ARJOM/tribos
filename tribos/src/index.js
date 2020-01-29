e7cc8f8f3db73ea520fa6c89be1231c6ea841440
Ricart <ricarteusuo11@gmail.com>
2020-01-29 19:52:03 -0300

    Alterando o index e atualizando o package.json e yarn.lock

--- tribos/src/index.js
+++ tribos/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {urls} from "./utils/urlUtils";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Route path={urls.home.path} component={App}/>
+    </Router>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
