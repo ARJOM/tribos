60e010e6bddce927d462a6b759f4aa52b1d8cf89
Ricart <ricarteusuo11@gmail.com>
2020-01-29 19:34:23 -0300

    Convertendo elementos do firebase para aplicação react

---
+++ tribos/src/services/FirebaseService.js
@@ -0,0 +1,32 @@
+import {firebaseDatabase, firebaseImpl} from '../utils/firebaseUtils'
+
+export default class FirebaseService {
+
+    static getDataList = (nodePath, callback, size = 10) => {
+
+        let query = firebaseDatabase.ref(nodePath)
+            .limitToLast(size);
+        query.on('value', dataSnapshot => {
+            let items = [];
+            dataSnapshot.forEach(childSnapshot => {
+                let item = childSnapshot.val();
+                item['key'] = childSnapshot.key;
+                items.push(item);
+            });
+            callback(items);
+        });
+
+        return query;
+    };
+
+    static pushData = (node, objToSubmit) => {
+        const ref = firebaseDatabase.ref(node).push();
+        const id = firebaseDatabase.ref(node).push().key;
+        ref.set(objToSubmit);
+        return id;
+    };
+
+    static logout = () => {
+        firebaseImpl.auth().signOut();
+    }
+}
